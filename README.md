19dc67be6a59ee68992e94ebcd5aaee8ba56e5d5
eddwang <30468094+eddwang@users.noreply.github.com>
2021-04-18 15:57:36 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+
+這是對APSO下面的 apso.oxt/python/scripts/tools.py 針對自己的喜好修改，在使用時你需要
+
+* [LibreOffice 7.1.2.2](https://zh-tw.libreoffice.org/)
+* [APSO Extension 1.2.6.2](https://gitlab.com/jmzambon/apso)
+* [MRI Extension 1.3.4](https://github.com/hanya/MRI)
+* [你的 python IDE](https://code.visualstudio.com/)
+* [對 LibreOffice python 巨集的了解](https://wiki.documentfoundation.org/Macros/Python_Guide/My_first_macro)
+
+
+
+
+
+
+
---
+++ tools.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+
+def console(*args, **kwargs):
+    '''Start the"python interpreter" gui
+    Allowed keyword arguments are:
+    'loc' : for passing caller's locales and/or globals to the console context
+    any constructor constant (BACKGROUND, FOREGROUND...) to tweak the console aspect
+    Examples:
+    - console()  # defaut constructor)
+    - console(loc=locals())
+    - console(BACKGROUND=0x0, FOREGROUND=0xFFFFFF)
+    '''
+    # we need to load apso before import statement
+    ctx = XSCRIPTCONTEXT.getComponentContext()
+    ctx.ServiceManager.createInstance("apso.python.script.organizer.impl")
+    # now we can use apso_utils library
+    from apso_utils import console
+    from pathlib import Path
+    from uno import fileUrlToSystemPath
+    import os
+    def execfile(python_file):
+        desktop = XSCRIPTCONTEXT.getDesktop()
+        doc = desktop.loadComponentFromURL(__file__, "_default", 0, ())
+        doc.CurrentController.Frame.ContainerWindow.Visible = True
+        doc.CurrentController.Frame.ContainerWindow.toFront()
+        exec(open(python_file).read())
+        return
+
+    desktop = XSCRIPTCONTEXT.getDesktop()
+    doc = desktop.getCurrentComponent()
+    __file__ = os.path.join(str(Path.home()),"python") if doc.Location == "" else doc.Location
+    os.chdir(fileUrlToSystemPath(os.path.dirname(__file__)))
+
+    kwargs.setdefault('loc', {})
+    kwargs['loc'].setdefault('XSCRIPTCONTEXT', XSCRIPTCONTEXT)
+    kwargs['loc'].setdefault('execfile', execfile)
+    kwargs['loc'].setdefault('__file__', __file__)
+    kwargs['loc'].setdefault('os', os)
+    kwargs.setdefault('BACKGROUND',0x0)
+    kwargs.setdefault('FOREGROUND',0xFFFFFF)
+    kwargs.setdefault('WIDTH',1000)
+    console(**kwargs)
+    
+g_exportedScripts = console,
+
